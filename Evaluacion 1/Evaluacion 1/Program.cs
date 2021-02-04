781585a5dc33e73364bae1b88cb940ed3d50183c
juanptorop <78567708+juanptorop@users.noreply.github.com>
2021-02-04 19:23:46 -0400

    Archivo Evaluación 1 Corregido
    Este SI es la evaluación 1, el otro archivo es un error

---
+++ Evaluacion 1/Evaluacion 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Evaluacion_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 50;
+            int b = 30;
+            int c = 78;
+            
+            //aacb
+            Console.WriteLine("{0}{0}{2}{1}",a,b,c);
+
+            //bca
+            Console.WriteLine("{1}{2}{0}", a, b, c);
+
+            //bcbc
+            Console.WriteLine("{1}{2}{1}{2}", a, b, c);
+            
+            Console.ReadKey();
+        }
+    }
+}
